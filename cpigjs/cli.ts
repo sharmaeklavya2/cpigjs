29af4b8d2a5e9d1888d0e5107cda4f32286d1c89
Eklavya Sharma <ekurgn@gmail.com>
2025-01-07 22:30:23 +0530

    robustify cli to incorrect predNames

--- cpigjs/cli.ts
+++ cpigjs/cli.ts
@@ -7,6 +7,12 @@ export interface Ostream {
 }
 
 export function outputPath(input: FilteredCpigInput, u: string, v: string, stdout: Ostream): void {
+    if(!input.predsMap.has(u)) {
+        throw new Error('unknown predicate ' + u);
+    }
+    if(!input.predsMap.has(v)) {
+        throw new Error('unknown predicate ' + v);
+    }
     const path = input.impG.getPath(u, v);
     if(path === undefined) {
         stdout.log(`no path from ${u} to ${v}`);
@@ -29,6 +35,9 @@ export function outputPath(input: FilteredCpigInput, u: string, v: string, stdou
 export function outputAttrReasons(input: FilteredCpigInput, predNames: string[], stdout: Ostream) {
     for(const [attrName, attrReasonsMap] of input.predAttrs.entries()) {
         for(const predName of predNames) {
+            if(!input.predsMap.has(predName)) {
+                throw new Error('unknown predicate ' + predName);
+            }
             const reasons = attrReasonsMap.getAll(predName);
             if(reasons.length > 0) {
                 stdout.log('');
